d60bfc9ba87893932bd7827011de0ba643305ff8
Taralas209 <alexej.zikunow@gmail.com>
2023-10-14 22:44:04 +0200

    download book covers

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import requests
 import os
-from requests.exceptions import HTTPError
+import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from requests.exceptions import HTTPError
 from pathvalidate import sanitize_filename
+from urllib.parse import urlsplit, unquote
+
 
 
 def check_for_redirect(response, book_url):
@@ -24,6 +27,20 @@ def get_book_title(book_url):
 
     return title
 
+def get_image_source(book_url):
+    base_url = 'https://tululu.org'
+    response = requests.get(book_url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    img_src = soup.find(class_='bookimage').find('img')['src']
+    if img_src:
+        image_link = urljoin(base_url, img_src)
+        print(f"Ссылка на изображение: {image_link}")
+    else:
+        print("Не удалось найти ссылку на изображение.")
+
+    return image_link
+
 def download_txt(response, title, path, book_id):
     sanitized_title = sanitize_filename(title)
     filename = f"{book_id}.{sanitized_title}.txt"
@@ -31,7 +48,20 @@ def download_txt(response, title, path, book_id):
         file.write(response.text)
 
 
-def main(url, download_url, path):
+def download_image(image_url, img_path, book_id):
+    response = requests.get(image_url)
+    response.raise_for_status()
+
+    split_url = urlsplit(image_url)
+    path = unquote(split_url.path)
+    extension = path.split('.')[-1] if '.' in path else None
+    imagename = f"{book_id}.{extension}"
+
+    with open(os.path.join(img_path, imagename), 'wb') as image:
+        image.write(response.content)
+
+
+def main(url, download_url, path, img_path):
     for book_id in range(1, 11):
         book_url = f"{url}{book_id}"
         book_url_to_download = f"{download_url}{book_id}"
@@ -46,12 +76,16 @@ def main(url, download_url, path):
             continue
 
         title = get_book_title(book_url)
+        image_link = get_image_source(book_url)
         download_txt(response, title, path, book_id)
+        download_image(image_link, img_path, book_id)
 
 
 if __name__ == "__main__":
     os.makedirs('books', exist_ok=True)
-    path = "./books/"
+    os.makedirs('images', exist_ok=True)
+    path = './books/'
+    img_path = './images/'
     url = 'https://tululu.org/b'
     download_url = 'https://tululu.org/txt.php?id='
-    main(url, download_url, path)
+    main(url, download_url, path, img_path)
