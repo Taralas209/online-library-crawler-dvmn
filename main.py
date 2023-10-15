c236bdee1b420c66ba01851c47ceecc8e8745fea
Taralas209 <alexej.zikunow@gmail.com>
2023-10-15 16:04:59 +0200

    download comments

--- main.py
+++ main.py
@@ -7,44 +7,35 @@ from pathvalidate import sanitize_filename
 from urllib.parse import urlsplit, unquote
 
 
-
 def check_for_redirect(response, book_url):
     if response.url != book_url or response.history:
         raise HTTPError(f"Redirection occurred from {book_url} to {response.url}")
 
 
-def get_book_title(book_url):
+def get_book_info(book_url, url):
     response = requests.get(book_url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
+
     title_tag = soup.find('h1')
-    if title_tag:
-        title_text = title_tag.text.split('::')
-        title = title_text[0].strip()
-        print(f"Заголовок: {title}")
-    else:
-        print("Не удалось найти заголовок.")
+    title = title_tag.text.split('::')[0].strip() if title_tag else None
 
-    return title
-
-def get_image_source(book_url):
-    base_url = 'https://tululu.org'
-    response = requests.get(book_url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
     img_src = soup.find(class_='bookimage').find('img')['src']
-    if img_src:
-        image_link = urljoin(base_url, img_src)
-        print(f"Ссылка на изображение: {image_link}")
-    else:
-        print("Не удалось найти ссылку на изображение.")
+    image_link = urljoin(url, img_src) if img_src else None
 
-    return image_link
+    comments_elements = soup.find('div', id="content").find_all('span', class_='black')
+    comments = [comment.text for comment in comments_elements]
 
-def download_txt(response, title, path, book_id):
+    return title, image_link, comments
+
+
+def download_txt(response, title, comments, path, book_id):
     sanitized_title = sanitize_filename(title)
     filename = f"{book_id}.{sanitized_title}.txt"
+    print(comments)
     with open(os.path.join(path, filename), 'w', encoding='utf-8') as file:
+        for comment in comments:
+            file.write(comment + '\n\n')
         file.write(response.text)
 
 
@@ -61,10 +52,20 @@ def download_image(image_url, img_path, book_id):
         image.write(response.content)
 
 
-def main(url, download_url, path, img_path):
-    for book_id in range(1, 11):
-        book_url = f"{url}{book_id}"
-        book_url_to_download = f"{download_url}{book_id}"
+def main(start_id=1, end_id=11):
+    path = './books/'
+    img_path = './images/'
+    url = 'https://tululu.org'
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    if not os.path.exists(img_path):
+        os.makedirs(img_path)
+
+    for book_id in range(start_id, end_id):
+        book_url = f"{url}/b{book_id}"
+        book_url_to_download = f"{url}/txt.php?id={book_id}"
 
         try:
             response = requests.get(book_url_to_download)
@@ -75,17 +76,10 @@ def main(url, download_url, path, img_path):
             print(f"Error occurred for book {book_id}: {e}")
             continue
 
-        title = get_book_title(book_url)
-        image_link = get_image_source(book_url)
-        download_txt(response, title, path, book_id)
+        title, image_link, comments = get_book_info(book_url, url)
+        download_txt(response, title, comments, path, book_id)
         download_image(image_link, img_path, book_id)
 
 
 if __name__ == "__main__":
-    os.makedirs('books', exist_ok=True)
-    os.makedirs('images', exist_ok=True)
-    path = './books/'
-    img_path = './images/'
-    url = 'https://tululu.org/b'
-    download_url = 'https://tululu.org/txt.php?id='
-    main(url, download_url, path, img_path)
+    main()
